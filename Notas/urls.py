80e5944ddb777b4cde31aff4410a15ba52eaa32f
Pablodlv <dlvpablo@hotmail.com>
2023-09-08 08:27:32 -0300

    error en listar notas

--- Notas/urls.py
+++ Notas/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from Notas.views import listadoNotas
+from Notas.views import notasLista
 
 urlpatterns = [
     
-    path('listadoNotas/', listadoNotas, name = "listadoNotas"),
+    #path('listadoNotas/', listadoNotas, name = "listadoNotas"),
+    path('notas/list/', notasLista.as_view(), name='listado_notas'),
 ]
